74809814b7b3f30a5a914a26266a68bab9bbf82e
danigj86 <48217639+danigj86@users.noreply.github.com>
2022-06-05 01:41:50 +0200

    Navbar & Carousel Components añadido

---
+++ src/components/Carrousel.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+export const Carrousel = () => {
+    return (
+        <div id="carouselExampleCaptions" className="carousel slide carousel-fade" data-bs-ride="carousel">
+            <div className="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div className="carousel-inner">
+                <div className="carousel-item active" data-bs-interval="2000">
+                    <img src="http://www.studioremarkable.com/wp-content/uploads/2012/08/Dark-Knight-Trilogy.jpg" className="d-block w-100" alt="..." />
+                    <div className="carousel-caption d-none d-md-block">
+                        <h5>First slide label</h5>
+                        <p>Some representative placeholder content for the first slide.</p>
+                    </div>
+                </div>
+                <div className="carousel-item" data-bs-interval="2000">
+                    <img src="https://i0.wp.com/hipertextual.com/wp-content/uploads/2020/05/hipertextual-star-wars-celebra-may-the-4th-be-with-you-con-extraordinario-video-que-rinde-homenaje-toda-saga-2020581511.jpg?fit=1200%2C675&quality=50&strip=all&ssl=1" className="d-block w-100" alt="..." />
+                    <div className="carousel-caption d-none d-md-block">
+                        <h5>Second slide label</h5>
+                        <p>Some representative placeholder content for the second slide.</p>
+                    </div>
+                </div>
+                <div className="carousel-item" data-bs-interval="2000">
+                    <img src="https://cdn.mos.cms.futurecdn.net/KKeszNADDpSVbVvxFRFxWY.jpg" className="d-block w-100" alt="..." />
+                    <div className="carousel-caption d-none d-md-block">
+                        <h5>Third slide label</h5>
+                        <p>Some representative placeholder content for the third slide.</p>
+                    </div>
+                </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
+        </div>
+    )
+}
