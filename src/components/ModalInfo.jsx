561c2ffe5a215fc275d4f23b3e0d060165e2dabe
danigj86 <48217639+danigj86@users.noreply.github.com>
2022-07-25 01:07:17 +0200

    Ver Trailer añadido

--- src/components/ModalInfo.jsx
+++ src/components/ModalInfo.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 export const ModalInfo = ({ id, title, overview, date, vote, poster }) => {
+
+const url = "https://www.youtube.com/results?search_query=trailer+" + title;
+
     return (
         <>
             <button type="button" className="btn btn-dark" data-bs-toggle="modal" data-bs-target={"#number" + id}>
@@ -20,6 +23,8 @@ export const ModalInfo = ({ id, title, overview, date, vote, poster }) => {
                                 <label className="form-label">{overview}</label>
                                 <label className="form-label">Year: {date}</label>
                                 <p className="form-label">Score: {vote}/10</p>
+                                <p>Trailer:  <a href={url} target="_blank" rel="noopener noreferrer">Click to see the trailer</a> </p>
+                                <p>Download Torrent (Bit Torrent)</p>
                             </div>
 
                         </div>
